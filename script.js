c3bbee08bdcf4778dbcac62e3a24b976084e1d86
K-M-Shehan <miweshshehan@gmail.com>
2026-06-05 11:50:18 +0530

    add play game

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const ROCK = 0;
 const PAPER = 1;
 const SCISSORS = 2;
 
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() { // randomly returns rock, paper or scissor
   let choice;
   let rand = Math.floor(Math.random() * 3) // will give a random value from 0 - 2 inclusive
@@ -29,26 +26,39 @@ function getHumanChoice() {
   return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-  if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "paper" || humanChoice == "paper" && computerChoice == "rock") {
-    console.log("You Win! " + humanChoice + " beats " + computerChoice);
-    humanScore++;
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "paper" || humanChoice == "paper" && computerChoice == "rock") {
+      console.log("You Win! " + humanChoice + " beats " + computerChoice);
+      humanScore++;
+    }
+    else if (humanChoice == computerChoice) {
+      console.log("It's a draw");
+    }
+    else {
+      console.log("You Lose! " + computerChoice + " beats " + humanChoice);
+      computerScore++;
+    }
   }
-  else if (humanChoice == computerChoice) {
-    console.log("Its a draw");
+
+  for (let i = 0; i < 5; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+
+  if (humanScore > computerScore) {
+    console.log("You won, Score: " + humanScore + "-" + computerScore);
+  }
+  else if (humanScore == computerScore) {
+    console.log("It's a draw, Score: " + humanScore + "-" + computerScore);
   }
   else {
-    console.log("You Lose! " + computerChoice + " beats " + humanChoice);
-    computerScore++;
+    console.log("You lost, Score: " + humanScore + "-" + computerScore);
   }
 }
 
-const human = getHumanChoice();
-const puter = getComputerChoice();
-
-console.log(human);
-console.log(puter);
-
-playRound(human, puter);
+playGame();
