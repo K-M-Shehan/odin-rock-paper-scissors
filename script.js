e39962d4d64610e5cfff54c9311f8a0fac3f9b01
K-M-Shehan <miweshshehan@gmail.com>
2026-06-07 21:43:19 +0530

    add buttons
    they call play round with correct selection

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const ROCK = 0;
 const PAPER = 1;
 const SCISSORS = 2;
 
+let humanScore = 0;
+let computerScore = 0;
+
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const sciButton = document.querySelector("#scissors");
+
 function getComputerChoice() { // randomly returns rock, paper or scissor
   let choice;
   let rand = Math.floor(Math.random() * 3) // will give a random value from 0 - 2 inclusive
@@ -21,35 +28,21 @@ function getComputerChoice() { // randomly returns rock, paper or scissor
   return choice;
 }
 
-function getHumanChoice() {
-  let choice = prompt("Enter your choice (rock, paper or scissors): ");
-  return choice;
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "paper" || humanChoice == "paper" && computerChoice == "rock") {
+    console.log("You Win! " + humanChoice + " beats " + computerChoice);
+    humanScore++;
+  }
+  else if (humanChoice == computerChoice) {
+    console.log("It's a draw");
+  }
+  else {
+    console.log("You Lose! " + computerChoice + " beats " + humanChoice);
+    computerScore++;
+  }
 }
 
 function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-
-    if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "paper" || humanChoice == "paper" && computerChoice == "rock") {
-      console.log("You Win! " + humanChoice + " beats " + computerChoice);
-      humanScore++;
-    }
-    else if (humanChoice == computerChoice) {
-      console.log("It's a draw");
-    }
-    else {
-      console.log("You Lose! " + computerChoice + " beats " + humanChoice);
-      computerScore++;
-    }
-  }
-
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-
   if (humanScore > computerScore) {
     console.log("You won, Score: " + humanScore + "-" + computerScore);
   }
@@ -61,4 +54,15 @@ function playGame() {
   }
 }
 
-playGame();
+// event listeners
+rockButton.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+})
+
+paperButton.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+});
+
+sciButton.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice());
+});
